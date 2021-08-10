75f3fdbf54e38c5fd974f8169693a98e69aebe98
pekojoe <85988618+pekojoe@users.noreply.github.com>
2021-08-10 12:07:51 +0900

    ユーザー認証機能のテスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :user do
+    japanese_user = Gimei.name
+
+    nickname              {Faker::Name.initials}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    family_name           {japanese_user.last.kanji}
+    first_name            {japanese_user.first.kanji}
+    family_name_kana      {japanese_user.last.katakana}
+    first_name_kana       {japanese_user.first.katakana}
+    birthday              {Faker::Date.birthday}
+  end
+end
